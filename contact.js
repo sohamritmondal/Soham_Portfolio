d25099c3c8c5be414a02bdbcf12166fbcaa686dd
sohamritmondal <sohamritmondal@gmail.com>
2026-06-12 14:48:47 +0530

    new layout

---
+++ contact.js
@@ -0,0 +1,13 @@
+const scriptURL = 'Your Google App Script URL'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  
+  e.preventDefault()
+  
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! Form is submitted" ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
